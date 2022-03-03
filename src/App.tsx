916d33383471e3fc7cdba328cc9a23526ea033f2
Vinicius <vinicius_prsilva@hotmail.com>
2022-03-03 18:55:51 -0300

    application structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
+import styled from 'styled-components';
+import {GlobalStyle} from './styles/global';
+
+
 export function App() {
   return (
-    <h2>Hello world!</h2>
+    <>
+    <h1>Hello world!</h1>
+    <GlobalStyle/>
+    </>
   );
 }
 
---
+++ src/styles/global.ts
@@ -0,0 +1,49 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+    :root {
+        --blue: #5429CC;
+        --blue-light: #6933ff;
+        --green: #33CC95;
+        --red: #E62E4D;
+        --white: #FFFFFF;
+        --title: #363F5F;
+        --text: #969CB3;
+        --background: #F0F2F5; 
+    }
+
+    html {
+        @media(max-width:1080px){
+            font-size: 93.75%;
+        }
+        @media(max-width: 720px){
+            font-size: 87,5%;
+        }
+    }
+    body{
+        background: var(--background);
+        -webkit-font-smoothing: antialiased;
+    }
+
+    body, button, textarea, input {
+        font-family: 'Poppins', sans-serif;
+        font-weight: 400;
+    }
+    h1,h2,h3,h4,h5,h6,strong{
+        font-weight: 600;
+    }
+
+    button {
+        cursor: pointer;
+    }
+
+    [disabled]{
+        opacity: 0.6;
+        cursor: not-allowed;
+    }
+`;
